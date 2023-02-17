e2333b3ac76cf6f86f9d1e35d0a50f4fd356425d
Nikola Jovanovic <nikolajovanovic5556@gmail.com>
2023-02-17 14:10:07 +0100

    init

--- src/utils/resources/mantineTheme.ts
+++ src/utils/resources/mantineTheme.ts
@@ -3,5 +3,5 @@ import { MantineThemeOverride } from '@mantine/core';
 import { THEME_MANTINE } from './themes/mantine';
 
 export const getMantineTheme = (): MantineThemeOverride => {
-  return THEME_MANTINE('dark', 'blue', 'Inter');
+  return THEME_MANTINE();
 };
